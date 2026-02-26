f098a6933848cc3b8191fb8a8a257977b4e27ea8
Harsh Yadav <yadavjiharsh.04@gmail.com>
2026-02-27 02:49:06 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 
 
 def home(request):
-  return render(request, 'home.html')
+  cateogries = Category.objects.all()
+  featured_post = Blog.objects.filter(is_featured = True, status= 'Published').order_by('updated_at')
+  posts = Blog.objects.filter(is_featured= False, status= 'Published')
+  context = {
+    'cateogries' : cateogries,
+    'featured_post' : featured_post,
+    'posts' : posts,
+  }
+  return render(request, 'home.html', context) 
